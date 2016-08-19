347024b01a8c945f813e7cae20bf07e3b3e08814
olecya <gxendz@mail.ru>
2016-08-19 12:03:30 +0300

    home_first

--- test.c
+++ test.c
@@ -6,6 +6,7 @@
 #include <locale.h>
 #include <unistd.h>
 #include <stdio_ext.h>
+#include <signal.h>
  
 struct termios oldt, newt;
 
@@ -13,13 +14,14 @@ int main()
 {
 	if (! setlocale(LC_ALL, ""))
 		perror("error locale"), exit(EXIT_FAILURE);
-	wchar_t bd[7][80] = {L"Антипов И С", L"Антипов П В", L"Антонов А С", L"Гузман М З", L"Звягин В Б", L"Язов А П", L"Нальгиев М В"};
+	wchar_t bd[7][80] = {L"Антипов И С", L"Антипов П В", L"Антонов А С", L"Гузман М З", L"Звягин В Б", L"Язов А П", L"Нальгиев Магамед Вахааевич"};
 
 	const wint_t c_term = L'\n';
 	wchar_t wch;
 	wchar_t flag;
 	wchar_t wchl[16];
 	int i, j, l, dest;
+	pid_t addr;
 	char sbuffstr[256];
 
 	tcgetattr(0, &oldt);
@@ -29,12 +31,7 @@ int main()
 	newt.c_cc[VTIME] = 1;
 	tcsetattr(0, TCSANOW, &newt);
 
-start:	sleep(1);  // сбросить буфер???
-//	stdin = freopen("/dev/stdin", "r", stdin);
-//	fflush(stdin);
-//	__fpurge(stdin);
-//	stdin = freopen("/dev/stdin", "r", stdin);
-	flag = L'a';
+start:	flag = L'a';
 	j = 0;
 	l = 0;
 	dest = 0;
@@ -55,11 +52,6 @@ start:	sleep(1);  // сбросить буфер???
 			break;
 		wcsncat(wchl, &wch, 1);
 
-//		stdout = freopen("/dev/stdout", "w", stdout);
-//		printf("%s", "\e[1;1H\e[2J");
-//		stdout = freopen("/dev/stdout", "w", stdout);
-//		system("clear");
-
 		for(i=0; i< sizeof(bd)/sizeof(bd[0]); i++)
 		{
 			if(! wcsncmp(bd[i], wchl, l))
@@ -77,23 +69,37 @@ start:	sleep(1);  // сбросить буфер???
 		else
 		{
 			wprintf(L"\e[1;1H\e[2J %ls\n\a", L"Не найдено или ошибка в наборе");
-//			usleep(100000);
+			if(!fork())
+				execlp("spd-say", "spd-say", "-l", "ru", "Стой! Стрелять буду!", NULL);
 //			break;
 			goto start;
 		}
 		putwchar(c_term);
 	}
-//	putwchar(c_term);
 	if(wch == c_term)
 	{
 		tcsetattr(0, TCSANOW, &oldt);
 		return 0;
 	}
-	wprintf(L"\e[1;1H\e[2J %ls %ls\a\n", bd[dest], L"Записано");
-	if(!fork())
+//	wprintf(L"\e[1;1H\e[2J %ls %ls\a\n", bd[dest], L"Записать в реестр?[Enter]:");
+	wprintf(L"\e[1;1H\e[2J %ls %ls\a\n", bd[dest], L"Записано в реестр!");
+	if(! (addr = fork()))
 	{
-		wcstombs(sbuffstr, bd[dest], 255);
-		execlp("spd-say", "spd-say", "-l", "ru", sbuffstr,  NULL);
+		wscanf(L"%*[^\n]");
+		wscanf(L"%*lc");
 	}
+	else
+	{
+		if(!fork())
+		{
+			wcstombs(sbuffstr, bd[dest], 255);
+			execlp("spd-say", "spd-say", "-l", "ru", sbuffstr,  NULL);
+		}
+		sleep(1);
+		kill(addr, 2);
+	}	
+//	while ( getwchar() != c_term);
+//	wprintf(L"%ls\n", L"Записано, готово к новому вводу:");
+
 	goto start;
 }
